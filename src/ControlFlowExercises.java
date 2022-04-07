ad57a149a83e887d629f8d7200a2ec99ea685960
Keith Benjamin <keith.i.benj@gmail.com>
2022-04-07 10:55:20 -0500

    Completed loop basics

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,9 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        1. Loop Basics
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i++);
+        }
+    }
+}
