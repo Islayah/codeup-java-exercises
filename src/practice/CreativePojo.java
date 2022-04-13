8670007436f64f407e85dbdc22d058afb401763a
Keith Benjamin <keith.i.benj@gmail.com>
2022-04-12 23:45:07 -0500

    added Pojoo warmup

---
+++ src/practice/CreativePojo.java
@@ -0,0 +1,33 @@
+//package practice;
+//
+//public class CreativePojo {
+////    Properties
+//    private String baseClass;
+//    private String advanceClass;
+//    public Boolean isMain;
+//    public int classLevel;
+//    public int rosterLevel;
+//
+////    Constructors
+//
+//    public CreativePojo(String baseClass, String advanceClass, int classLevel, int rosterLevel, Boolean isMain) {
+//        this.baseClass = baseClass;
+//        this.advanceClass = advanceClass;
+//        this.classLevel = classLevel;
+//        this.rosterLevel = rosterLevel;
+//        this.isMain = isMain;
+//    }
+//
+////    Getters and Setters
+//
+//    public String getClasses() {
+//        return this.baseClass + " " + this.advanceClass;
+//    }
+//
+//    public void setClasses() {
+//        this.baseClass + this.advanceClass =
+//    }
+//
+////    Custom Methods
+//
+//}
